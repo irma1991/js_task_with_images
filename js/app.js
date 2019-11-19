fd9b46aa47f54d6bba14399668cab3c03e5ba832
nomaxas <nomaxas@gmail.com>
2019-11-19 11:26:24 +0200

    files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let images = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9'];
+
+const mainPlace = document.querySelector('ul')
+const buttonFirst = document.getElementById('buttonFirst');
+const buttonSecond = document.getElementById('buttonSecond');
+
+buttonFirst.addEventListener('click', showImages);
+buttonSecond.addEventListener('click', mixImages);
+
+function showImages(){
+	for(i=0; i< images.lenght; i++){
+		const imagePlace = document.querySelector('li')
+	}
+}
+
+
