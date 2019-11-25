bda47941230b02ef81d12a30b41db8c29ddcdc26
Irma Suchonickaja <irmalinartaite@gmail.com>
2019-11-25 21:13:21 +0200

    files

--- js/app.js
+++ js/app.js
@@ -1,16 +1,60 @@
-let images = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9'];
+const vietos = document.querySelector('ul');
 
-const mainPlace = document.querySelector('ul')
-const buttonFirst = document.getElementById('buttonFirst');
-const buttonSecond = document.getElementById('buttonSecond');
+const button1 = document.querySelector('#rodyti');
+button1.addEventListener('click', RodykFoto)
 
-buttonFirst.addEventListener('click', showImages);
-buttonSecond.addEventListener('click', mixImages);
+const button2 = document.querySelector('#maisyti');
+button2.addEventListener('click', MaisykFoto)
 
-function showImages(){
-	for(i=0; i< images.lenght; i++){
-		const imagePlace = document.querySelector('li')
-	}
+let foto = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9', 'img10'];
+
+function RodykFoto()
+{
+	for (let i = 0; i < foto.length; i++)
+	{
+		const vieta = document.createElement('li');
+        const fotos = document.createElement("img");
+        fotos.src='img/'+foto[i]+'.png';
+        vietos.appendChild(vieta);
+        vieta.appendChild(fotos);
+    }
 }
+
+function displayNone()
+{
+    let x = document.getElementById("rodyti");
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function MaisykFoto()
+{
+	let naujas = foto
+    naujas.sort(function () {
+        return 0.5 - Math.random()
+});
+
+const seni = document.querySelectorAll('img');
+	for(let i=0; i<naujas.length; i++)
+	{
+		seni[i].src='img/'+naujas[i]+'.png'
+    }
+
+    const list = document.querySelectorAll('img');
+    for(let i=0; i < list.length; i++)
+    {
+    	list[i].ondblclick = function (e)
+    	{
+    		let target = e.target
+            target.src = 'img/cover.png';
+        }
+    }
+}
+
+
 
 
